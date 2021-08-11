81569b7eff8b793be17f92fd86ef7f01d909ece0
luiseduCosta <luiseduardocosta417@gmail.com>
2021-08-11 11:18:17 -0300

    first commit

---
+++ src/test/java/br/ufma/lsdi/CarrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufma.lsdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
