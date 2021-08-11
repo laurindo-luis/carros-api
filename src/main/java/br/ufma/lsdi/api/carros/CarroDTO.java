876a02f4e33968b0eb6a37cc7cd663aeb87606dc
luiseduCosta <luiseduardocosta417@gmail.com>
2021-08-11 12:33:25 -0300

    class CarroDTO added

---
+++ src/main/java/br/ufma/lsdi/api/carros/CarroDTO.java
@@ -0,0 +1,19 @@
+package br.ufma.lsdi.api.carros;
+
+import org.modelmapper.ModelMapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CarroDTO {
+	
+	private Long id;
+	private String modelo;
+	private String cor;
+	
+	public CarroDTO create(Carro carro) {
+		return new ModelMapper().map(carro, CarroDTO.class);
+	}
+}
