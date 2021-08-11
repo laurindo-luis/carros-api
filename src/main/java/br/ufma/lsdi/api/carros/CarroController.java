c6e3583d061b9a04f8338e64ece352bf42ac8ebc
luiseduCosta <luiseduardocosta417@gmail.com>
2021-08-11 15:33:51 -0300

    method post

--- src/main/java/br/ufma/lsdi/api/carros/CarroController.java
+++ src/main/java/br/ufma/lsdi/api/carros/CarroController.java
@@ -1,12 +1,18 @@
 package br.ufma.lsdi.api.carros;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/v1/carros")
@@ -16,8 +22,34 @@ public class CarroController {
 	private CarroService carroService;
 	
 	@GetMapping
-	public ResponseEntity getCarro() {
+	public ResponseEntity<?> getCarro() {
 		List<CarroDTO> carros = carroService.getCarros();
 		return ResponseEntity.ok(carros);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCarroById(@PathVariable("id") Long id) {
+		
+		return carroService.getCarroById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> salvar(@RequestBody Carro carro) {
+		
+		try {
+			CarroDTO carroDTO = carroService.salvar(carro);
+			return carroDTO == null ? ResponseEntity.badRequest().build() : 
+				ResponseEntity.created(getURI(carroDTO.getId())).build();
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	private URI getURI(Long id) {
+		return ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}")
+				.buildAndExpand(id).toUri();
 	}
 }
--- src/main/java/br/ufma/lsdi/api/carros/CarroService.java
+++ src/main/java/br/ufma/lsdi/api/carros/CarroService.java
@@ -1,8 +1,10 @@
 package br.ufma.lsdi.api.carros;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.internal.util.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,21 @@ public class CarroService {
 	private CarroRepository carroRepository;
 	
 	public List<CarroDTO> getCarros() {
-		
 		return carroRepository.findAll()
 				.stream()
 				.map(CarroDTO::create)
 				.collect(Collectors.toList());
+	}
+	
+	public Optional<CarroDTO> getCarroById(Long id) {
+		return carroRepository
+				.findById(id)
+				.map(CarroDTO::create);
+	}
+	
+	public CarroDTO salvar(Carro carro) {
+		Assert.isNull(carro.getId(), "Não foi possível inserir o registro!");
+		
+		return CarroDTO.create(carroRepository.save(carro));
 	}
 }
